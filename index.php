f675805ed6e58f21f556af1ee38beea501f70cbb
Apollo2000 <apollo.iptv@gmail.com>
2015-12-24 11:30:33 +0200

    Fix quote in binary mysql field

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Base on OpenTracker WhitSoft project (2009)
  * By BitTorrent Tracker Protocol (https://wiki.theory.org)
  * Need: Linux / PHP 5.5+ / Apache / MYSQL
- * Version 1.00
+ * Version 1.01
  */
 
 // Block browsers get info from tracker
@@ -26,12 +26,13 @@ if ($action["path"]!= '/scrape')
 	elseif (!isset($_GET['info_hash']))		$failure_code = 101;
 	elseif (!isset($_GET['peer_id']))		$failure_code = 102;
 	elseif (!isset($_GET['port']) || !is_numeric($_GET['port']))	$failure_code = 103;
-	elseif (strlen($_GET['info_hash'])!=20)	$failure_code = 150;
+	elseif (strlen($_GET['info_hash'])!=20 && strlen($_GET['info_hash'])!=40)	$failure_code = 150;
 	elseif (strlen($_GET['peer_id'])!=20)	$failure_code = 151;
 	elseif (!isset($_GET['uploaded']) || !is_numeric($_GET['uploaded']) || !isset($_GET['downloaded']) || !is_numeric($_GET['downloaded']) || !isset($_GET['left']) || !is_numeric($_GET['left']) || (!empty($_GET['event']) && ($_GET['event'] != 'started') && ($_GET['event'] != 'completed') && ($_GET['event'] != 'stopped')))
 		$failure_code = 900;
+
 	if ($failure_code)	sendError($failure_code);
-		
+
 	$ip = getUserIP();
 	if ($ip === false) exit("Unable to resolve ip");
 
@@ -45,19 +46,18 @@ if ($action["path"]!= '/scrape')
 	$DB = mysqli_connect($db_server, $db_user, $db_pass,$db_db) or exit("DB Error: Can't connect - ".mysqli_connect_error());
 
 	// Remove stopped peer
-	if ($_GET['event'] == 'stopped'){
-		mysqli_query($DB,"DELETE FROM `$db_table` WHERE `info_hash` ='".$_GET["info_hash"]."' AND `peer_id`='".$_GET["peer_id"]."';") or exit("DB Error: Can't run DELETE - ".mysqli_connect_error());
-		exit;
-	}
+	if ($_GET['event'] == 'stopped')
+		mysqli_query($DB,sprintf("DELETE FROM `$db_table` WHERE `info_hash` ='%s' AND `peer_id`='%s';",mysqli_real_escape_string($DB,$_GET['info_hash']),mysqli_real_escape_string($DB,$_GET["peer_id"]))) or exit("DB Error: Can't run DELETE - ".mysqli_connect_error());
 
 	// Update peer in DB
 	$columns = '`info_hash`, `ip`, `port`, `peer_id`, `uploaded`, `downloaded`, `left`, `update_time`';
-	$values = "'".$_GET["info_hash"]."', INET_ATON('$ip'), '".$_GET['port']."', '".$_GET["peer_id"]."', '".$_GET['uploaded']."', '".$_GET['downloaded']."', '$left', NOW()";
-	mysqli_query($DB,"REPLACE INTO `$db_table` ($columns) VALUES ($values);") or exit('database error');
+	$values = sprintf("'%s', INET_ATON('$ip'), '".$_GET['port']."', '%s', '".$_GET['uploaded']."', '".$_GET['downloaded']."', '$left', NOW()",mysqli_real_escape_string($DB,$_GET['info_hash']),mysqli_real_escape_string($DB,$_GET["peer_id"]));
+	$replace = ("REPLACE INTO `$db_table` ($columns) VALUES ($values);");
+	mysqli_query($DB,$replace) or exit("DB Error: Can't run REPLACE - ".mysqli_connect_error());
 
 	// Get peers info from DB
 	$max_peers = (isset($_GET["numwant"]) && is_numeric($_GET["numwant"]) && ($_GET["numwant"]>0) && ($_GET["numwant"]<$max_ann_peers)?$_GET["numwant"]:$max_ann_peers);
-	$query = mysqli_query($DB,"SELECT INET_NTOA(`ip`) as `ip`, `port`, `peer_id` FROM `$db_table` WHERE `info_hash` = '".$_GET["info_hash"]."' AND `update_time` > NOW() - INTERVAL $expire_time MINUTE ORDER BY RAND() LIMIT $max_peers;") or exit("DB Error: Can't run QUERY - ".mysqli_connect_error());
+	$query = mysqli_query($DB,sprintf("SELECT INET_NTOA(`ip`) as `ip`, `port`, `peer_id` FROM `$db_table` WHERE `info_hash` = '%s' AND `update_time` > NOW() - INTERVAL $expire_time MINUTE ORDER BY RAND() LIMIT $max_peers;",mysqli_real_escape_string($DB,$_GET['info_hash']))) or exit("DB Error: Can't run QUERY - ".mysqli_connect_error());
 
 	// Peers output method
 	$peers = Array();							
@@ -75,11 +75,11 @@ if ($action["path"]!= '/scrape')
 			$peers[] = array('ip' => $array['ip'], 'port' => intval($array['port']), 'peer id' => $array['peer_id']);
 
 	// Get seed&leech info from DB
-	$query = mysqli_query($DB,"SELECT `info_hash`, 
+	$query = mysqli_query($DB,sprintf("SELECT `info_hash`, 
 			SUM(CASE WHEN `left` =0 AND `update_time` > NOW() - INTERVAL $expire_time MINUTE THEN 1 ELSE 0 END) AS complete, 
 			SUM(CASE WHEN `left` !=0 AND `update_time` > NOW() - INTERVAL $expire_time MINUTE THEN 1 ELSE 0 END) AS incomplete,
 			SUM(CASE WHEN `left` =0 THEN 1 ELSE 0 END) AS downloaded
-			FROM `$db_table` WHERE `info_hash`='".$_GET["info_hash"]."' GROUP BY `info_hash`;") or exit("DB Error: Can't run QUERY - ".mysqli_connect_error());
+			FROM `$db_table` WHERE `info_hash`='%s' GROUP BY `info_hash`;",mysqli_real_escape_string($DB,$_GET['info_hash']))) or exit("DB Error: Can't run QUERY - ".mysqli_connect_error());
 	$result = mysqli_fetch_array($query,MYSQL_ASSOC);
 
 	// Send results to client
@@ -88,8 +88,8 @@ if ($action["path"]!= '/scrape')
 else // Scrape MOD
 {
 	// Check validate info_hash
-	if (isset($_GET['info_hash']) && strlen($_GET['info_hash'])!=20) sendError(150);
-	
+	if (isset($_GET['info_hash']) && strlen($_GET['info_hash'])!=40 && strlen($_GET['info_hash'])!=20) sendError(150);
+	$info_hash = ($_GET['info_hash']==40)?hex2bin($_GET['info_hash']):$_GET['info_hash'];
 	// Connect to DB
 	$DB = mysqli_connect($db_server, $db_user, $db_pass,$db_db) or exit("DB Error: Can't connect - ".mysqli_connect_error());
 
@@ -99,7 +99,7 @@ else // Scrape MOD
 			SUM(CASE WHEN `left` =0 AND `update_time` > NOW() - INTERVAL $expire_time MINUTE THEN 1 ELSE 0 END) AS complete, 
 			SUM(CASE WHEN `left` !=0 AND `update_time` > NOW() - INTERVAL $expire_time MINUTE THEN 1 ELSE 0 END) AS incomplete,
 			SUM(CASE WHEN `left` =0 THEN 1 ELSE 0 END) AS downloaded
-			FROM `$db_table` ".(!empty($_GET['info_hash'])?"WHERE HEX(`info_hash`)='".$_GET["info_hash"]."'":"")." GROUP BY `info_hash`;") 
+			FROM `$db_table` ".(!empty($_GET['info_hash'])?sprintf("WHERE `info_hash`='%s'",mysqli_real_escape_string($DB,$_GET['info_hash'])):"")." GROUP BY `info_hash`;") 
 			or exit("DB Error: Can't run QUERY - ".mysqli_connect_error());
 	while ($row = mysqli_fetch_array($query,MYSQL_ASSOC)) $results[] = $row;
 
