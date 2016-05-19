66ac6c82ba49a1b668ceadfc0cdfbb110b372b79
Apollo2000 <apollo.iptv@gmail.com>
2016-05-19 12:38:15 +0300

    Add Event

--- dbschema.sql
+++ dbschema.sql
@@ -1,14 +1,15 @@
 CREATE TABLE `STT_Peers` (
   `info_hash` binary(20) NOT NULL,
   `peer_id` binary(20) NOT NULL,
-  `ip` INT UNSIGNED NOT NULL,
+  `ip` int(10) unsigned NOT NULL,
   `port` smallint(5) unsigned NOT NULL,
   `uploaded` bigint(20) unsigned NOT NULL default '0',
   `downloaded` bigint(20) unsigned NOT NULL default '0',
   `left` bigint(20) unsigned NOT NULL default '0',
+  `event` varchar(10) DEFAULT NULL,
   `update_time` timestamp NOT NULL,
   PRIMARY KEY  (`info_hash`,`ip`,`port`)
-) ENGINE=HEAP;
+) ENGINE=HEAP DEFAULT CHARSET=utf8;
 
 CREATE EVENT `STT_Clean` 
 ON SCHEDULE EVERY 10 MINUTE 
